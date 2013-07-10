06306682118bc7b581084ec13741f493db359740
Charles Rodetsky <crodetsky@gmail.com>
2013-07-10 18:55:48 -0400

    Added getToday function

--- jobrun2/jobrun2.py
+++ jobrun2/jobrun2.py
@@ -27,6 +27,27 @@ class JobRun2:
             return e
         return status
 
+    def getToday(self, rk):
+        today = datetime.strptime(datetime.today().strftime('%m/%d/%Y 00:00:00'), '%m/%d/%Y %H:%M:%S')
+        tomorrow = today + timedelta(1)
+        s = 0
+        try:
+            jlookup = self.jl.get(rk, column_start=tomorrow, column_finish=today)
+        except Exception, e:
+            return e
+        try:
+            statuses = self.jr.multiget(jlookup.values(), column_start='status', column_finish='status')
+        except Exception, e:
+            return e
+            
+        for status in statuses.values():
+            if status['status'] == 0:
+                s += 1
+        print s
+        print len(statuses)
+        success_rate = (float(s)/float(len(statuses))) * 100
+        return success_rate
+
     def getJobKeys(self):
 	self.rks = []
         jobs = self.jl.get_range(column_count=0, filter_empty=False)
