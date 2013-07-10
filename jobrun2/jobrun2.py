30a7809c38ff367881a00e31e860c70474d0ae8d
Kenneth Hopkins <ken@72-31-239-158.net.bhntampa.com>
2013-07-10 12:19:58 -0400

    adding JobRun2 class to git

--- jobrun2/jobrun2.py
+++ jobrun2/jobrun2.py
@@ -1,76 +1,76 @@
 #!/usr/bin/env python
-
-from flask import Flask, request
-from uuid import uuid4
 from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
-import datetime
+from datetime import datetime, timedelta
 
-app = Flask(__name__)
-
-pool = ConnectionPool('jobrun', server_list=['tamp20-seot-cas1.bhn.net:9160',
+class JobRun2:
+    def __init__(self):
+	err = ''
+        self.pool = ConnectionPool('jobrun', server_list=['tamp20-seot-cas1.bhn.net:9160',
                                              'tamp20-seot-cas2.bhn.net:9160',
                                              'tamp20-seot-cas3.bhn.net:9160',])
-jl = ColumnFamily(pool, 'job_lookup')
-jr = ColumnFamily(pool, 'job_results')
+        self.jl = ColumnFamily(self.pool, 'job_lookup')
+        self.jr = ColumnFamily(self.pool, 'job_results')
 
-@app.route('/jobrun2/record_job', methods=['POST'])
-def record_job():
-    jobresults = {}
-    print request.form
-    started = datetime.datetime.strptime(request.form['started'], '%m/%d/%Y %H:%M:%S.%f')
-    jobresults['dataset'] = request.form['dataset']
-    jobresults['action'] = request.form['action']
-    jobresults['status'] = int(request.form['status'])
-    jobresults['output'] = request.form['output']
-    if request.form['command']:
-        jobresults['command'] = request.form['command']
-    if request.form['username']:
-        jobresults['username'] = request.form['username']
-    if request.form['program']:
-        jobresults['program'] = request.form['program']
-    if request.form['machine']:
-        jobresults['machine'] = request.form['machine']
+    def closePool(self):
+        pass	
 
-    jobid = uuid4()
-    year = int(datetime.datetime.strftime(started, '%Y'))
-    rk = (jobresults['dataset'], jobresults['action'], year)
-    jl.insert(rk, {started: jobid})
-    jr.insert(jobid, jobresults)
-    return('OK')
+    def getLast(self,rk):
+	self.status = 'bobo'
+        #try:
+        self.jlookup = self.jl.get(rk, column_count=1)
+        print "Jlookup = %s" % (self.jlookup)
+        self.status = self.jr.get(self.jlookup.values()[0], column_start='status', column_finish='status')['status']
+	print self.status
+        #except Exception,e:
+        #    print e
+        #    return e
+        return self.status
 
-@app.route('/jobrun2')
-def show_jobs():
-    j = {}
-    jobs = jl.get_range(column_count=0)
-    for job in jobs:
-        rk = job[0]
-        last = getLast(rk)
-        success7 = getSuccess(rk, 7)
-        success14 = getSuccess(rk, 14)
-        success30 = getThirtyDaySuccess(rk, 30)
-        j[rk] = {'last': last, 'success7': success7, 'success14': success14, 'success30': success30}
+    def getJobKeys(self):
+	self.rks = []
+        jobs = self.jl.get_range(column_count=0, filter_empty=False)
+        for job in jobs:
+            self.rks.append(job[0])
+	return self.rks
 
-def getLast(rk):
-    jlookup = jl.get(column_reversed=True, column_count=1)
-    status = jr.get(jlookup.values()[0], column_start='status', column_finish='status')['status']
-    return status
+#    def getSuccess(sefl,rk,days):
+#        start = datetime.now()
+#        stop = start-timedelta(days)
+#        statusSum = 0.0
+#        numjobs = 0
+#        try:
+#            jlookup = jl.get(rk, column_start=start, column_finish=stop)
+#        except:
+#            return None
+#        job_statuses = jr.multiget(jlookup.values(), column_start='status', column_finish='status')
+#        #print statuses
+#        try:
+##             numjobs = len(job_statuses)
+#        except:
+#             return 0.0
+#        for status in statuses.values():
+#             #if days == 7: print status['status']
+#             if status['status'] == 0:
+#                statusSum += 1
+#        #if days == 7: print statusSum, numjobs
+#        if statusSum != 0 and numjobs != 0:
+#            success = (float(statusSum) / float(numjobs)) * 100
+#        else:
+#            success = 0.0
+#        return success
 
-def getSuccess(rk, days):
-    start = datetime.datetime.now()
-    stop = start-datetime.timedelta(days)
-    statusSum = 0
-    jlookup = jl.get(column_start=start, column_finish=stop)
-    statuses = jr.multiget(jlookup.volues(), column_start='status', column_finish='status')
-    try:
-        numjobs = len(statuses)
-    except:
-        return 0.0
-    for status in statuses:
-        statusSum += status['status']
-    success = (float(statusSum) / float(numjobs)) * 100
-    return success
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+#j = {}
+#jobs = jl.get_range(column_count=0, filter_empty=False)
+#for job in jobs:
+#    rk = job[0]
+##    print rk
+#    last = getLast(rk)
+#    success7 = getSuccess(rk, 7)
+#    success14 = getSuccess(rk, 14)
+#    success30 = getSuccess(rk, 30)
+##    print rk, last, success7, success14, success30
+#    if success7 != None and success14 != None and success30 != None:
+#        j[rk] = {'last': last, 'success7': success7, 'success14': success14, 'success30': success30}
+#
+#print j
