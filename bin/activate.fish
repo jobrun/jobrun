0fbfd52281ae96c3054e71921ed8f3574879348f
Charles Rodetsky <crodetsky@gmail.com>
2013-07-18 12:10:34 -0400

    fixed paths

--- bin/activate.fish
+++ bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/crodetsk/.virtualenvs/jobrun2"
+set -gx VIRTUAL_ENV "/Users/crodetsk/.virtualenvs/jtasks"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
